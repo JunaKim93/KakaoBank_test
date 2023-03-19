bf64243a115531373760375a58c68505803517c2
junatan <kja3369@gmail.com>
2023-03-19 21:44:30 +0900

    commit2

--- src/main/java/com/school/Main.java
+++ src/main/java/com/school/Main.java
@@ -3,6 +3,11 @@ package com.school;
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,6 +15,7 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,6 +23,36 @@ public class Main {
         Main main = new Main();
 
         List <String> replyList = main.readCsv();
+
+        List<Map<String, Object>> schoolList = main.getSchoolList();
+
+        for(String reply : replyList) {
+            String [] words = reply.split(" ");
+
+            for(String word : words){
+                if(word.contains("중학교") || word.contains("고등학교")){
+                    System.out.println(word);
+                }
+            }
+        }
+    }
+
+    private List<Map<String, Object>> getSchoolList() {
+        List<Map<String, Object>> schoolList = new ArrayList<>();
+        try {
+            String url = "https://www.edufindkorea.com/edu/list.php?schoolgubun=%EA%B3%A0%EB%93%B1%ED%95%99%EA%B5%90";
+            Connection connection = Jsoup.connect(url);
+
+            Document document = connection.get();
+
+            Elements schoolNames = document.select("table.datatable12 tbody tr td div a");
+            for(Element element : schoolNames) {
+                System.out.println(element.text());
+            }
+        } catch (Exception e) {
+            System.out.println("[ getSchoolList ERROR ] =========  " + e.getMessage());
+        }
+        return schoolList;
     }
 
     private List<String> readCsv() {
